b998a2bc22c9a8cd418e4f80dd670e6c5a146173
Arunadevi <l.arunadevi@gmail.com>
2016-03-23 20:20:50 +0530

    First commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+var themingApp = angular.module('themingClient', []);
+
+themingApp.factory('themingFactory', function() {
+	var themingFactory = {};
+	return themingFactory;
+});
+themingApp.controller('themingCtrl',
+	function($scope, $cookies, signInFactory,$window,$http) {
+
+});
+
