7ee6060517b1b52769996e23278ae93271d4b532
Arunadevi <l.arunadevi@gmail.com>
2016-03-24 14:01:21 +0530

    Themes rail and geo

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
-/**
- * 
- */
-var themingApp = angular.module('themingClient', []);
+var defaultTheme = 'rail';
+var themingApp = angular.module('themingClient', ['ui.bootstrap']);
 
 themingApp.factory('themingFactory', function() {
 	var themingFactory = {};
 	return themingFactory;
 });
 themingApp.controller('themingCtrl',
-	function($scope, $cookies, signInFactory,$window,$http) {
-
+	function($scope, $modal) {
+	$scope.theme = defaultTheme;
+	$scope.openModal = function(){
+var modalInstance = $modal.open({
+			templateUrl: 'myModalContent.html',
+			controller: 'themingModalInstanceCtrl',
+			windowClass: 'nm-compmail-modal mail-body',
+			backdrop: 'static',
+			resolve: {
+				theme: function () {
+					return $scope.theme;
+				}
+			}
+		});
+}
 });
 
+
+themingApp.controller('themingModalInstanceCtrl', function ($scope, $modalInstance, theme) {
+	$scope.theme = theme;
+});
