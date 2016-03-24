24f6175602d14eab96df3185316b9ca126012e6f
Arunadevi <l.arunadevi@gmail.com>
2016-03-24 14:08:23 +0530

    All themes

--- js/script.js
+++ js/script.js
@@ -2,28 +2,28 @@ var defaultTheme = 'rail';
 var themingApp = angular.module('themingClient', ['ui.bootstrap']);
 
 themingApp.factory('themingFactory', function() {
-	var themingFactory = {};
-	return themingFactory;
+    var themingFactory = {};
+    return themingFactory;
 });
 themingApp.controller('themingCtrl',
-	function($scope, $modal) {
-	$scope.theme = defaultTheme;
-	$scope.openModal = function(){
-var modalInstance = $modal.open({
-			templateUrl: 'myModalContent.html',
-			controller: 'themingModalInstanceCtrl',
-			windowClass: 'nm-compmail-modal mail-body',
-			backdrop: 'static',
-			resolve: {
-				theme: function () {
-					return $scope.theme;
-				}
-			}
-		});
-}
-});
+    function($scope, $modal) {
+        $scope.theme = defaultTheme;
+        $scope.openModal = function() {
+            var modalInstance = $modal.open({
+                templateUrl: 'myModalContent.html',
+                controller: 'themingModalInstanceCtrl',
+                windowClass: 'nm-compmail-modal mail-body',
+                backdrop: 'static',
+                resolve: {
+                    theme: function() {
+                        return $scope.theme;
+                    }
+                }
+            });
+        }
+    });
 
 
-themingApp.controller('themingModalInstanceCtrl', function ($scope, $modalInstance, theme) {
-	$scope.theme = theme;
+themingApp.controller('themingModalInstanceCtrl', function($scope, $modalInstance, theme) {
+    $scope.theme = theme;
 });
